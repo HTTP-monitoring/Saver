da5219ae1b61a08b94a5eb5ba708bf521fde2ebd
raha <elahe.dstn@gmail.com>
2020-07-24 17:31:17 +0430

    Add config

---
+++ config/default.go
@@ -0,0 +1,19 @@
+package config
+
+const Default = `
+db:
+  host: 127.0.0.1
+  port: "5431"
+  user: postgres
+  dbname: monitor
+  password: postgres
+  sslmode: disable
+redis:
+  host: 127.0.0.1
+  port: "6378"
+  threshold: 4
+nats:
+  host: nats://localhost:4221
+  topic: monitor
+  queue: monitors
+`
